157b1683e2c649398b692b4a7fc5203ccadf23a2
Ekky Kharismadhany <ekharisma@outlook.com>
2024-08-05 09:07:09 +0700

    envoy lb

---
+++ app/measure_time.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# URL to test
+url="localhost:9090/echo?iter=10000"
+
+# Use curl to measure the time of the request
+start=$(date +%s%3N)  # Start time in milliseconds
+curl -s $url > /dev/null  # Perform the request
+end=$(date +%s%3N)    # End time in milliseconds
+
+# Calculate duration
+duration=$((end - start))
+
+# Output the duration
+echo "Request time: ${duration} ms"
